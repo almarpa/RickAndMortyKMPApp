23830de7940a4f52d1f2cad66947d7590dfeb986
amarcopa <amarcopa@mercadona.es>
2024-10-07 20:22:05 +0200

    feature(bottom-navhost): add background color

--- composeApp/src/commonMain/kotlin/com/almarpa/rickandmortyapp/ui/core/navigation/BottomNavHost.kt
+++ composeApp/src/commonMain/kotlin/com/almarpa/rickandmortyapp/ui/core/navigation/BottomNavHost.kt
@@ -1,11 +1,14 @@
 package com.almarpa.rickandmortyapp.ui.core.navigation
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.almarpa.rickandmortyapp.ui.home.tabs.characters.CharactersScreen
 import com.almarpa.rickandmortyapp.ui.home.tabs.episodes.EpisodesScreen
+import com.almarpa.rickandmortyapp.ui.theme.BackgroundPrimaryColor
 
 @Composable
 fun BottomNavHost(
@@ -13,8 +16,9 @@ fun BottomNavHost(
     navigationActions: NavigationActions,
 ) {
     NavHost(
+        modifier = Modifier.background(BackgroundPrimaryColor),
         navController = navController,
-        startDestination = Routes.Characters
+        startDestination = Routes.Characters,
     ) {
         composable<Routes.Characters> {
             CharactersScreen(
