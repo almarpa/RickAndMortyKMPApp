2f18ce39fe8570c7572a1bf6a28b265da7fdd979
amarcopa <amarcopa@mercadona.es>
2024-10-07 20:06:14 +0200

    feature(splash): add ios implementation

--- composeApp/src/commonMain/kotlin/com/almarpa/rickandmortyapp/ui/core/components/TopBar.kt
+++ composeApp/src/commonMain/kotlin/com/almarpa/rickandmortyapp/ui/core/components/TopBar.kt
@@ -1,12 +1,12 @@
 package com.almarpa.rickandmortyapp.ui.core.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.almarpa.rickandmortyapp.ui.theme.BackgroundPrimaryColor
@@ -14,16 +14,21 @@ import org.jetbrains.compose.resources.painterResource
 import rickandmortyapp.composeapp.generated.resources.Res
 import rickandmortyapp.composeapp.generated.resources.ricktoolbar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
-    Box(
-        modifier = Modifier.fillMaxWidth().background(BackgroundPrimaryColor),
-        contentAlignment = Alignment.TopCenter
-    ) {
-        Image(
-            painter = painterResource(Res.drawable.ricktoolbar),
-            null,
-            modifier = Modifier.padding(start = 16.dp, top = 32.dp, bottom = 8.dp)
-        )
-    }
+fun TopBar(scrollBehaviour: TopAppBarScrollBehavior) {
+    CenterAlignedTopAppBar(
+        title = {
+            Image(
+                painter = painterResource(Res.drawable.ricktoolbar),
+                contentDescription = null,
+                modifier = Modifier.padding(vertical = 32.dp)
+            )
+        },
+        navigationIcon = {},
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = BackgroundPrimaryColor,
+        ),
+        scrollBehavior = scrollBehaviour,
+    )
 }
--- composeApp/src/commonMain/kotlin/com/almarpa/rickandmortyapp/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/almarpa/rickandmortyapp/ui/home/HomeScreen.kt
@@ -2,9 +2,13 @@ package com.almarpa.rickandmortyapp.ui.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.compose.rememberNavController
 import com.almarpa.rickandmortyapp.ui.core.components.BottomBar
 import com.almarpa.rickandmortyapp.ui.core.components.TopBar
@@ -13,14 +17,24 @@ import com.almarpa.rickandmortyapp.ui.core.navigation.BottomBarItem.Episodes
 import com.almarpa.rickandmortyapp.ui.core.navigation.BottomNavHost
 import com.almarpa.rickandmortyapp.ui.core.navigation.NavigationActions
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navigationActions: NavigationActions) {
-    val items = listOf(Episodes(), Characters())
+    val items = listOf(Characters(), Episodes())
     val bottomNavController = rememberNavController()
+    val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
-        topBar = { TopBar() },
-        bottomBar = { BottomBar(items, bottomNavController) },
+        modifier = Modifier.nestedScroll(scrollBehaviour.nestedScrollConnection),
+        topBar = {
+            TopBar(scrollBehaviour = scrollBehaviour)
+        },
+        bottomBar = {
+            BottomBar(
+                items = items,
+                navController = bottomNavController
+            )
+        },
     ) { padding ->
         Box(modifier = Modifier.padding(padding)) {
             BottomNavHost(bottomNavController, navigationActions)
